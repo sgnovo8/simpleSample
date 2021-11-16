ad10d1ed7be396a974bcf393efdd8cd14b49fbf4
sgnovo8 <88598099+sgnovo8@users.noreply.github.com>
2021-11-16 18:43:01 -0500

    Update simpleSample.sol

--- simpleSample.sol
+++ simpleSample.sol
@@ -1,8 +1,27 @@
 pragma solidity ^0.4.7;
 
 contract simpleSample {
+
+// declare a state variable
+ 
     uint stateVar;
     
+    // or
+    
+    string stateVar2;
+    
+    // or
+    
+    bool stateVar3;
+
+// create a function that does something to stateVar like modifiy it in some way
+// how about one that sets whatever you want as the stateVar state variable?
+//
+
+    function set(string z) public {
+        stateVar2 = z;
+    }
+
     function set(uint x) public {
         stateVar = x;
     }
